f78603883bf978da0c1d9d795f705b4461916b84
Lavakumar Veludandi <lavakumar4422@gmail.com>
2022-11-09 17:27:33 +0100

    changes update

---
+++ SalesMVC/Controllers/SalesController.cs
@@ -0,0 +1,41 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class SalesController: Controller
+	{
+        private readonly ILogger<HomeController> _logger;
+
+        private static List<SalesAgentViewModel> salesAgents = new List<SalesAgentViewModel>();
+
+        public SalesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Agents()
+        {
+            return View(salesAgents);
+        }
+
+        public IActionResult Create()
+        {
+            SalesAgentViewModel salesAgentViewModel = new SalesAgentViewModel();
+            return View(salesAgentViewModel );
+        }
+
+        public IActionResult CreateAgent(SalesAgentViewModel salesAgentViewModel)
+        {
+            salesAgents.Add(salesAgentViewModel);
+            return RedirectToAction(nameof(Agents));
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
